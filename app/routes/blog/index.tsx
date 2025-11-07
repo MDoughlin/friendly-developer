a0819bf76237bba497cf61e4f83740ac25efe78d
Micheline <MDoughlin@hotmail.com>
2025-11-06 22:47:25 -0500

    Updated Blog Detail Page

--- app/routes/blog/index.tsx
+++ app/routes/blog/index.tsx
@@ -1,6 +1,9 @@
 import type { Route } from "./+types/index";
 import type { PostMeta } from "~/types";
 import PostCard from "../components/PostCard";
+import Pagination from "../components/Pagination";
+import PostFilter from "../components/PostFilter";
+import { useState } from "react";
 
 export async function loader({
   request,
@@ -12,18 +15,57 @@ export async function loader({
   if (!res.ok) throw new Error("Failed to fetch data");
 
   const data = await res.json();
+  data.sort((a: PostMeta, b: PostMeta) => {
+    return new Date(b.date).getTime() - new Date(a.date).getTime();
+  });
 
   return { posts: data };
 }
 
 const BlogPage = ({ loaderData }: Route.ComponentProps) => {
+  const [currentPage, setCurrentPage] = useState(1);
+  const [searchQuery, setSearchQuery] = useState("");
+  const postPerPage = 3;
+
   const { posts } = loaderData;
+  const filteredPosts = posts.filter((post) => {
+    const query = searchQuery.toLowerCase();
+    return (
+      post.title.toLowerCase().includes(query) ||
+      post.excerpt.toLowerCase().includes(query)
+    );
+  });
+
+  const totalPages = Math.ceil(filteredPosts.length / postPerPage);
+  const indexOfLast = currentPage * postPerPage;
+  const indexOfFirst = indexOfLast - postPerPage;
+  let currentPosts = filteredPosts.slice(indexOfFirst, indexOfLast);
+
   return (
     <div className="max-w-3xl mx-auto mt-10 px-6 py-6 bg-gray-900">
       <h2 className="text-3xl font-bol text-white mb-8 text-center">Blog</h2>
-      {posts.map((post) => (
-        <PostCard key={post.id} post={post} />
-      ))}
+      <PostFilter
+        searchQuery={searchQuery}
+        onSearchChange={(query) => {
+          setSearchQuery(query);
+          setCurrentPage(1);
+        }}
+      />
+      <div className="space-y-8">
+        {currentPosts.length === 0 ? (
+          <p className="text-gray-400 text-center">No post found</p>
+        ) : (
+          currentPosts.map((post) => <PostCard key={post.id} post={post} />)
+        )}
+      </div>
+      {}
+      {totalPages > 1 && (
+        <Pagination
+          currentPage={currentPage}
+          totalPages={totalPages}
+          onPageChange={(page) => setCurrentPage(page)}
+        />
+      )}
     </div>
   );
 };
